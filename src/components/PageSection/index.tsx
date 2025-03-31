a31666ea8d6ae6ab7c6a319966a2e0f32fe11fae
Bruno Tenório <tenoriobn@gmail.com>
2025-03-31 12:26:05 -0300

    04 - otimizando as renderizações da aplicação

--- src/components/PageSection/index.tsx
+++ src/components/PageSection/index.tsx
@@ -1,8 +1,10 @@
+import { memo } from "react";
+
 type PageSectionProps = {
 	children: React.ReactNode;
 };
 
-const PageSection: React.FC<PageSectionProps> = ({ children }) => {
+const PageSectionComponent: React.FC<PageSectionProps> = ({ children }) => {
 	return (
 		<div className='bg-[url("./bg-section.png")] h-96'>
 			<section className='flex flex-col items-center justify-center h-96 bg-gradient-to-r from-[#002F52] to-[#024271] to-95% w-full opacity-90'>
@@ -12,4 +14,5 @@ const PageSection: React.FC<PageSectionProps> = ({ children }) => {
 	);
 };
 
+const PageSection = memo(PageSectionComponent);
 export default PageSection;
--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/Header';
 import PageContent from '../../components/PageContent';
 import PageSection from '../../components/PageSection';
-import React, { lazy, Profiler, Suspense, useEffect } from 'react';
+import React, { lazy, Profiler, Suspense, useEffect, useMemo } from 'react';
 import { fetchBooks, filterItems } from '../../store/reducers/books';
 import { AppDispatch, RootState } from '../../store/store';
 import { Footer } from '../../components/Footer';
@@ -14,6 +14,12 @@ const Catalog: React.FC = () => {
 	const dispatch = useDispatch<AppDispatch>();
 	const [filterInput, setFilterInput] = React.useState('');
 	const { books, filteredBooks } = useSelector((state: RootState) => state.books);
+
+	const CatalogHeader = useMemo(() => (
+		<Header>
+			<img alt='ByteBooks Logo' src='./logo.png' height={70} />
+		</Header>
+	), []);
 
 	const showingItems = filteredBooks && filteredBooks.length > 0 ? filteredBooks : books;
 
@@ -40,47 +46,44 @@ const Catalog: React.FC = () => {
 	return (
 		<Profiler id='catalog' onRender={onRender}>
 			<React.Fragment>
+				{CatalogHeader}
+
+				<PageSection>
+					<h2 className='text-4xl text-white font-bold'>Já sabe por onde começar?</h2>
+					<h3 className='text-base text-white font-bold mt-4'>
+						Encontre em nossa estante o que precisa para seu desenvolvimento!
+					</h3>
+					<div className='relative mt-4 rounded-md text-center p-4 w-[350px] md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:w-1/2'>
+						<input
+							placeholder='Qual será sua próxima leitura?'
+							className='rounded-3xl text-base border-white hover:border-[#B87900] focus-visible:border-[#B87900] border text-center p-4 w-full focus:placeholder-transparent focus:text-left text-white bg-transparent placeholder-white'
+							onChange={handleSearch}
+							value={filterInput}
+						/>
+					</div>
+				</PageSection>
 				<React.Fragment>
-					<Header>
-						<img alt='ByteBooks Logo' src='./logo.png' height={70} />
-					</Header>
-					<PageSection>
-						<h2 className='text-4xl text-white font-bold'>Já sabe por onde começar?</h2>
-						<h3 className='text-base text-white font-bold mt-4'>
-							Encontre em nossa estante o que precisa para seu desenvolvimento!
-						</h3>
-						<div className='relative mt-4 rounded-md text-center p-4 w-[350px] md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:w-1/2'>
-							<input
-								placeholder='Qual será sua próxima leitura?'
-								className='rounded-3xl text-base border-white hover:border-[#B87900] focus-visible:border-[#B87900] border text-center p-4 w-full focus:placeholder-transparent focus:text-left text-white bg-transparent placeholder-white'
-								onChange={handleSearch}
-								value={filterInput}
+					{!filteredBooks?.length && filterInput.length > 0 ? (
+						<div className='flex-1 items-center mt-4'>
+							<h2 className='text-center text-[#002F52] text-[32px]'>
+								Oops! Não encontramos nenhum resultado.
+							</h2>
+							<img
+								src='/not_found.png'
+								alt='sem resultado'
+								className='w-1/2 max-w-[500px] mx-auto mt-4'
 							/>
 						</div>
-					</PageSection>
-					<React.Fragment>
-						{!filteredBooks?.length && filterInput.length > 0 ? (
-							<div className='flex-1 items-center mt-4'>
-								<h2 className='text-center text-[#002F52] text-[32px]'>
-									Oops! Não encontramos nenhum resultado.
-								</h2>
-								<img
-									src='/not_found.png'
-									alt='sem resultado'
-									className='w-1/2 max-w-[500px] mx-auto mt-4'
-								/>
+					) : (
+						<PageContent>
+							<div className='flex flex-wrap justify-center container items-start'>
+								<Suspense fallback={<img src="/loading.gif" alt="loading" />}>								
+									<BooksList items={showingItems} />
+								</Suspense>	
 							</div>
-						) : (
-							<PageContent>
-								<div className='flex flex-wrap justify-center container items-start'>
-									<Suspense fallback={<img src="/loading.gif" alt="loading" />}>								
-										<BooksList items={showingItems} />
-									</Suspense>	
-								</div>
-							</PageContent>
-						)}
-						<Footer />
-					</React.Fragment>
+						</PageContent>
+					)}
+					<Footer />
 				</React.Fragment>
 			</React.Fragment>
 		</Profiler>
